f6740b93ab81c10e7256abd806855046158e0e0f
aarondarwish <mail.aaron.darwish@gmail.com>
2022-08-07 11:58:37 +0100

    Import the needed libraries

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
-}
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_dynamodb::Client;
+use aws_sdk_dynamodb::model::AttributeValue;
+use lambda_runtime::{handler_fn}
+
+fn main() {}
