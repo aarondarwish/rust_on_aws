8abf5be1d5583076a020bf2271a3054a181b585d
Aaron Darwish <aaron7darwish@gmail.com>
2022-12-14 14:09:59 +0000

    Change the architecture to amd64.

--- rust_lambda_helpers.zsh
+++ rust_lambda_helpers.zsh
@@ -1,5 +1,5 @@
-LAMBDA_ARCH="linux/arm64" # set this to either linux/arm64 for ARM functions, or linux/amd64 for x86 functions.
-RUST_TARGET="aarch64-unknown-linux-gnu" # corresponding with the above, set this to aarch64 or x86_64 -unknown-linux-gnu for ARM or x86 functions.
+LAMBDA_ARCH="linux/amd64" # set this to either linux/arm64 for ARM functions, or linux/amd64 for x86 functions.
+RUST_TARGET="x86_64-unknown-linux-gnu" # corresponding with the above, set this to aarch64 or x86_64 -unknown-linux-gnu for ARM or x86 functions.
 RUST_VERSION="latest" # Set this to a specific version of rust you want to compile for, or to latest if you want the latest stable version.
 PROJECT_NAME="rust_on_aws"
 
